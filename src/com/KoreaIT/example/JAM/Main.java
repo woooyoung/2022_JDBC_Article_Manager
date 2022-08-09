79ed02649677fb1a918d716e62ce9b09e36fe080
woooyoung <axdsw121@gmail.com>
2022-08-09 17:21:29 +0900

    게시물 리스팅 시 데이터를 DB로부터 가져오기

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -3,6 +3,7 @@ package com.KoreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		List<Article> articles = new ArrayList<>();
 		int lastArticleId = 0;
 
 		while (true) {
@@ -28,7 +28,6 @@ public class Main {
 				String body = sc.nextLine();
 
 				Article article = new Article(id, title, body);
-				articles.add(article);
 
 				Connection conn = null;
 				PreparedStatement pstmt = null;
@@ -43,8 +42,8 @@ public class Main {
 					String sql = "INSERT INTO article";
 					sql += " SET regDate = NOW()";
 					sql += ", updateDate = NOW()";
-					sql += ", title = '" + title +"'";
-					sql += ", `body` = '" + body +"'";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "'";
 
 					System.out.println(sql);
 
@@ -80,6 +79,66 @@ public class Main {
 				System.out.println(article);
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				ResultSet rs = null;
+
+				List<Article> articles = new ArrayList<>();
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/article_manager?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC";
+
+					System.out.println(sql);
+
+					pstmt = conn.prepareStatement(sql);
+					rs = pstmt.executeQuery();
+
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
